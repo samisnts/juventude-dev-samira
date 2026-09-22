c177a9aba11c968168ebfa8637d3664d37ebc626
samisnts <sami.oliveira2010@gmail.com>
2026-09-22 19:15:02 +0000

    concluida

---
+++ aula-09/exemplo/teste.js
@@ -0,0 +1,12 @@
+function verificaridade(idade) {
+    if (idade >= 18) {
+        console.log("maior de idade");
+    }
+    else if(idade===30){
+        console.log("mais")
+    }
+    else {
+        console.log("menor de idade");
+    }
+}
+verificaridade(15)
